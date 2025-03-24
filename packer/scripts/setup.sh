8f61ed39c285eb94e88844b5a59c21fface2e85f
william <batchayonwilliam@gmail.com>
2025-03-24 04:51:20 +0100

    Script exécuté dans la VM pour installer Docker et Minikube

---
+++ packer/scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker.io curl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo usermod -aG docker ubuntu
+sudo systemctl enable docker
